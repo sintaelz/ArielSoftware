9ee538f5d54387b9e6df36e6701b4407e374f6f3
Seung Lee <sintaelz@gmail.com>
2016-08-18 15:47:10 -0500

    Tarea #2 agregada

---
+++ Tarea 2/Computadoras.h
@@ -0,0 +1,116 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+class Computadoras
+{
+public:
+  virtual void printComponentes() = 0;
+  virtual void printEnsamblado() = 0;
+  virtual void printInstalacion() = 0;
+  virtual void printEmpaquetado() = 0;
+  void crearDispositivo()
+  {
+    printComponentes();
+    printEnsamblado();
+    printInstalacion();
+    printEmpaquetado();
+  }
+};
+
+class Laptop : public Computadoras
+{
+  friend class concreteCreator;
+private:
+  Laptop(){};//En privado para que nadie las pueda utilizar mas que el admin
+public:
+  void printComponentes()
+  {
+      cout << "Juntando componentes necesarios para construir una Laptop..." << endl << endl;
+  }
+  void printEnsamblado()
+  {
+    cout << "Ensamblando componentes para la Laptop..."  << endl << endl;
+  }
+  void printInstalacion()
+  {
+    cout << "Instalando software necesario para la Laptop..."  << endl << endl;
+  }
+  void printEmpaquetado()
+  {
+    cout << "Empaquetando la Laptop :)" << endl;
+  }
+};
+
+class Desktop : public Computadoras
+{
+  friend class concreteCreator;
+private:
+  Desktop(){}; //En privado para que nadie las pueda utilizar mas que el admin
+public:
+  void printComponentes()
+  {
+      cout << "Juntando componentes necesarios para construir una Desktop..." << endl << endl;
+  }
+  void printEnsamblado()
+  {
+    cout << "Ensamblando componentes para la Desktop..."  << endl << endl;
+  }
+  void printInstalacion()
+  {
+    cout << "Instalando software necesario para la Desktop..."  << endl << endl;
+  }
+  void printEmpaquetado()
+  {
+    cout << "Empaquetando la Desktop :)" << endl;
+  }
+};
+
+class Tablet : public Computadoras
+{
+  friend class concreteCreator;
+private:
+  Tablet(){}; //En privado para que nadie las pueda utilizar mas que el admin
+public:
+  void printComponentes()
+  {
+      cout << "Juntando componentes necesarios para construir una Tablet..." << endl << endl;
+  }
+  void printEnsamblado()
+  {
+    cout << "Ensamblando componentes para la Tablet..."  << endl << endl;
+  }
+  void printInstalacion()
+  {
+    cout << "Instalando software necesario para la Tablet..."  << endl << endl;
+  }
+  void printEmpaquetado()
+  {
+    cout << "Empaquetando la Tablet :)" << endl;
+  }
+};
+
+class Netbook : public Computadoras
+{
+  friend class concreteCreator;
+private:
+  Netbook(){}; //En privado para que nadie las pueda utilizar mas que el admin
+public:
+  void printComponentes()
+  {
+      cout << "Juntando componentes necesarios para construir una Netbook..." << endl << endl;
+  }
+  void printEnsamblado()
+  {
+    cout << "Ensamblando componentes para la Netbook..."  << endl << endl;
+  }
+  void printInstalacion()
+  {
+    cout << "Instalando software necesario para la Netbook..."  << endl << endl;
+  }
+  void printEmpaquetado()
+  {
+    cout << "Empaquetando la Netbook :)" << endl;
+  }
+};
